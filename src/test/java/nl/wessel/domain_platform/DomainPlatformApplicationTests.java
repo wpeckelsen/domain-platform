c18b6a7b5a058b474ce7368591c6f39548cbf4bf
wpeckelsen <wpeckelsen@gmail.com>
2022-06-09 16:17:55 +0200

    start

---
+++ src/test/java/nl/wessel/domain_platform/DomainPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.wessel.domain_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
